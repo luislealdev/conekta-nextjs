c4ad3697ec4916b792db18a216956457a6297337
Luis Leal <luisrrleal@gmail.com>
2024-04-01 09:21:23 -0600

    Api accepts info from post

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -1,24 +1,26 @@
 'use server';
 import { CustomersApi, Configuration, Customer, CustomerResponse } from "conekta";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
 
 
 const apikey = process.env.CONEKTA_API_KEY;
 const config = new Configuration({ accessToken: apikey });
 const client = new CustomersApi(config);
 
-const customer: Customer = {
-    name: "John Constantine",
-    email: "frank@google.com",
-    phone: "+5215555555555"
-}
 
-export async function GET(req: NextRequest, res: NextResponse) {
-
+export async function POST(req: NextRequest, res: NextResponse) {
     try {
+        const { name, email, phone } = await req.json();
+
+        // console.log(req);
+        const customer: Customer = {
+            name,
+            email,
+            phone
+        }
         const resp = await client.createCustomer(customer);
-        // const customerResponse = resp.data as CustomerResponse;
-        // return new NextResponse(customerResponse.id, { status: 200 });
+        const customerResponse = resp.data as CustomerResponse;
+        return Response.json(customerResponse);
         // return res.status(200).send(customerResponse.id);
     } catch (error) {
         console.error(error);
